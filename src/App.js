513a1a2d92b5a047abb730741b811ee268f6698e
VanshajNarayan <vihaanp748@gmail.com>
2023-12-03 15:58:37 +0530

    add: addsomelogic

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   const [newsApi, setNewsApi] = useState([]);
   const newsApiFx = async () => {
     const response = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=50ee585bf1364c1db2d0b89b0b504a7b");
-    const data = await response.json();
-    let articlesData = data.articles;
-    setNewsApi(articlesData);
+    if (response.status >= 200 && response.status < 300) {
+      const data = await response.json();
+      let articlesData = data.articles;
+      setNewsApi(articlesData);
+    };
   };
   useEffect(() => {
     newsApiFx();
